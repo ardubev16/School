609f529d29eb14df93a4a6408669dae42d147d5e
ardubev16 <legolory@gmail.com>
2018-09-26 11:59:42 +0200

    commit try

---
+++ C/esercizi/180926_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int cell(){
+
+}
+
+int main(int argc, char const *argv[]) {
+  int 
+  return 0;
+}
+
+
+//inserire un numero, visualizzare le singole celle di memoria del numero con una funzione

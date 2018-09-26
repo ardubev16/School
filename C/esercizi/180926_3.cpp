e2ab213e7ab7da607ba0f63b01e72da8dc621907
ardubev16 <legolory@gmail.com>
2018-09-26 13:58:54 +0200

    ex3 09/26

--- C/esercizi/180926_3.cpp
+++ C/esercizi/180926_3.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
-int cell(){
-
+int cell(int a){
+  char *pa=(char*)&a;
+  printf("Indirizzi del numero e corrispettivi valori:\n");
+  for (int i = 0; i<sizeof(a); i++) {
+    printf("%d %d\n",pa+i, *(pa+i));
+  }
 }
 
 int main(int argc, char const *argv[]) {
-  int 
+  int a;
+  printf("Inserire il numero di cui visualizzare le celle di memoria\nn=");
+  scanf("%d",&a);
+  cell(a);
   return 0;
 }
 
